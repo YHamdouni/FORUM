678f537c521bfe0c958a90ee3fde39b41f333ae4
YHamdouni <168558215+YHamdouni@users.noreply.github.com>
2024-11-04 17:02:59 +0100

    v1

--- Handlers/Files/handlercss.go
+++ Handlers/Files/handlercss.go
@@ -7,7 +7,7 @@ import (
 )
 
 func StaticFiles(w http.ResponseWriter, r *http.Request) {
-	FileServer := http.StripPrefix("/styles/", http.FileServer(http.Dir("../../templates/styles/")))
+	FileServer := http.StripPrefix("/styles/", http.FileServer(http.Dir("../templates/styles/")))
 	if strings.HasSuffix(r.URL.Path, "/") {
 		Pages.ErrorHandler(w, http.StatusNotFound)
 		return
